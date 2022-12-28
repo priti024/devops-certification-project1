0de30c06e4c021aee4623a1bf47bf73f90d78226
priti024 <114428392+priti024@users.noreply.github.com>
2022-12-28 16:34:21 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region     = "ap-south-1"
 }
 resource "aws_instance" "Test_Server" {
-  ami           = ami-08eec94255d45165b
+  ami           = "ami-08eec94255d45165b"
   key_name =  "Mumbai"
   instance_type = "t2.micro"
   user_data = file("docker.sh")
